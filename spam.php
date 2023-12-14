67b271b7b7a4c72283cd938a452441c6ea44fe57
Rud Az <36195757+Lyaxpm@users.noreply.github.com>
2023-12-14 15:02:57 +0700

    Update spam.php

--- spam.php
+++ spam.php
@@ -67,7 +67,7 @@ class Frey {
     curl_setopt($curl, CURLOPT_URL, self::URL . $url);
     curl_setopt($curl, CURLOPT_POST, 1);
     curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($params));
-    curl_setopt($curl, CURLOPT_HTTPHEADER, ["x-apikey: " . $key]);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, ["X-Apikey: " . $key]);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
     $result = curl_exec($curl);
     curl_close($curl);
